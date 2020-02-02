699f9c4f326fd3009addb43761a8829a2e335848
LIOKKI <54829434+LIOKKI@users.noreply.github.com>
2020-02-02 15:31:30 +0800

    Update 学习使用github.php
    第一次补充学习内容

--- 学习使用github.php
+++ 学习使用github.php
@@ -8,4 +8,13 @@ pull request 发起请求独立编写的内容用于补充fork来的对象
 watch 关注：项目更新后可以收到通知
 
 Ch2：
-填写文件名需要加后缀？eg:学习使用github.php
+填写文件名需要加后缀？例如：学习使用github.php
+点击文件名可以删除修改文件内容
+点击文件描述（initial commit）查看该文件提交的详细信息
+upload files 从本地上传各种多个files
+find files 输入文件名称找到开源项目具体文件
+setting 用于对仓库进行操作
+delete this repository 删除这个仓库
+
+Ch3:
+下载安装git托管管理github
